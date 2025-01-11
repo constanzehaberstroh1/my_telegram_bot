6ae3467322322d64404163d40b1f01d82bee87eb
Salman Jabbari <s.jabbari@armansolutions.com>
2025-01-11 22:00:49 +0330

    Initial commit -3

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-if __name__ == '__main__':
-    print('Hello, World!')
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome, Clever"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
